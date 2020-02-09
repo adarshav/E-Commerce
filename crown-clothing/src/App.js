678d6c122e2cbe0123a6c9a85f21c8de64bbb0a6
adarsha <adarshav957@gmail.com>
2020-02-09 22:21:44 +0530

    changes

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage.component.js/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>This is Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <Switch>
+        <Route exact path = '/' component = {HomePage}/>
+        <Route  path = '/hats' component = {HatsPage}/>
+      </Switch>
+      {/* switch executes only one and not more */}
     </div>
   );
 }
